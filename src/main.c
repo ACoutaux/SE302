46be6c855529e5dd8cec4157d2364be976dfdaa6
agathe-coutaux <agathe.coutaux@telecom-paris.fr>
2022-10-14 09:40:40 +0200

    Adding a pause

--- src/main.c
+++ src/main.c
@@ -11,59 +11,74 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
+#include <zephyr/drivers/led.h>
 
-#define SLEEP_TIME_MS	1
+#define SLEEP_TIME_MS 1
 
 /*
  * Get button configuration from the devicetree sw0 alias. This is mandatory.
  */
-#define SW0_NODE	DT_ALIAS(sw0)
+#define SW0_NODE DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static struct gpio_callback button_cb_data;
 
 /*
  * The led0 devicetree alias is optional. If present, we'll use it
  * to turn on the LED whenever the button is pressed.
  */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+// Define work object
+struct k_work_delayable put_led_off_work;
+
+// Define function to be linked to the work object
+void put_led_off(struct k_work_delayable *item)
+{
+	gpio_pin_set_dt(&led, 0);
+}
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+					uint32_t pins)
 
 // Interruption routine
 {
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-  
-  gpio_pin_set_dt(&led, gpio_pin_get_dt(&button)); //set the led at button pin state
+	// gpio_pin_set_dt(&led, gpio_pin_get_dt(&button)); //set the led at button pin state
+	gpio_pin_set_dt(&led, 1);
+	k_work_reschedule(&put_led_off_work, K_SECONDS(1)); //reschedule() replaces any incomplete previous delay (unlike schedule())
 }
 
 void main(void)
 {
 	int ret;
 
-	if (!device_is_ready(button.port)) {
+	// Init work object by passing a reference to the function
+	k_work_init_delayable(&put_led_off_work, put_led_off);
+
+	if (!device_is_ready(button.port))
+	{
 		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+			   button.port->name);
 		return;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
+	if (ret != 0)
+	{
 		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
+			   ret, button.port->name, button.pin);
 		return;
 	}
 
 	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_BOTH); // GPIO_INT_EDGE_BOTH interrupts both when rising and falling edges
-	if (ret != 0) {
+										  GPIO_INT_EDGE_BOTH); // GPIO_INT_EDGE_BOTH interrupts both when rising and falling edges
+	if (ret != 0)
+	{
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
+			   ret, button.port->name, button.pin);
 		return;
 	}
 
@@ -71,18 +86,23 @@ void main(void)
 	gpio_add_callback(button.port, &button_cb_data);
 	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
-	if (led.port && !device_is_ready(led.port)) {
+	if (led.port && !device_is_ready(led.port))
+	{
 		printk("Error %d: LED device %s is not ready; ignoring it\n",
-		       ret, led.port->name);
+			   ret, led.port->name);
 		led.port = NULL;
 	}
-	if (led.port) {
+	if (led.port)
+	{
 		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-		if (ret != 0) {
+		if (ret != 0)
+		{
 			printk("Error %d: failed to configure LED device %s pin %d\n",
-			       ret, led.port->name, led.pin);
+				   ret, led.port->name, led.pin);
 			led.port = NULL;
-		} else {
+		}
+		else
+		{
 			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
 		}
 	}
@@ -90,7 +110,7 @@ void main(void)
 	printk("Press the button\n");
 	/*if (led.port) {
 		while (1) {
-			// If we have an LED, match its state to the button's. 
+			// If we have an LED, match its state to the button's.
 			int val = gpio_pin_get_dt(&button);
 
 			if (val >= 0) {
