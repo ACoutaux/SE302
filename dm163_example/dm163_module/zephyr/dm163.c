fa767684e98da5068d80bc0fe0c4154c28eea798
agathe-coutaux <agathe.coutaux@telecom-paris.fr>
2022-10-24 10:40:48 +0200

    WIP

--- dm163_example/dm163_module/zephyr/dm163.c
+++ dm163_example/dm163_module/zephyr/dm163.c
@@ -10,7 +10,8 @@
 
 LOG_MODULE_REGISTER(dm163, LOG_LEVEL_DBG);
 
-struct dm163_config {
+struct dm163_config
+{
   const struct gpio_dt_spec en;
   const struct gpio_dt_spec gck;
   const struct gpio_dt_spec lat;
@@ -19,23 +20,39 @@ struct dm163_config {
   const struct gpio_dt_spec sin;
 };
 
-#define CONFIGURE_PIN(dt, flags)                                               \
-  do {                                                                         \
-    if (!device_is_ready((dt)->port)) {                                        \
-      LOG_ERR("device %s is not ready", (dt)->port->name);                     \
-      return -ENODEV;                                                          \
-    }                                                                          \
-    gpio_pin_configure_dt(dt, flags);                                          \
-  } while (0)
+#define NUM_LEDS 8
+#define NUM_CHANNELS (NUM_LEDS * 3)
 
-static int dm163_init(const struct device *dev) {
+struct dm163_data
+{
+  uint8_t brightness[NUM_CHANNELS];
+  uint8_t channels[NUM_CHANNELS];
+};
+
+#define CONFIGURE_PIN(dt, flags)
+do
+{
+  if (!device_is_ready((dt)->port))
+  {
+    LOG_ERR("device %s is not ready", (dt)->port->name);
+    return -ENODEV;
+  }
+  gpio_pin_configure_dt(dt, flags);
+} while (0)
+
+    static int
+    dm163_init(const struct device *dev)
+{
   const struct dm163_config *config = dev->config;
+
+  struct dm163_data *data = dev->data; // define data and retrieve it from dev device
 
   LOG_DBG("starting initialization of device %s", dev->name);
 
   // Disable DM163 outputs while configuring if this pin
   // is connected.
-  if (config->en.port) {
+  if (config->en.port)
+  {
     CONFIGURE_PIN(&config->en, GPIO_OUTPUT_INACTIVE);
   }
   // Configure all pins. Make reset active so that the DM163
@@ -49,8 +66,16 @@ static int dm163_init(const struct device *dev) {
   k_usleep(1); // 100ns min
   // Cancel reset by making it inactive.
   gpio_pin_set_dt(&config->rst, 0);
+
+  // Init data and send it to DM163
+  memset(&data->brightness, 0x3f, sizeof(data->brightness));
+  memset(&data->channels, 0x00, sizeof(data->channels));
+  flush_brightness(dev);
+  flush_channels(dev);
+
   // Enable the outputs if this pin is connected.
-  if (config->en.port) {
+  if (config->en.port)
+  {
     gpio_pin_set_dt(&config->en, 1);
   }
   LOG_INF("device %s initialized", dev->name);
@@ -58,22 +83,73 @@ static int dm163_init(const struct device *dev) {
 }
 
 // Macro to initialize the DM163 peripheral with index i
-#define DM163_DEVICE(i)                                                        \
-                                                                               \
-  /* Build a dm163_config for DM163 peripheral with index i, named          */ \
-  /* dm163_config_/i/ (for example dm163_config_0 for the first peripheral) */ \
-  static const struct dm163_config dm163_config_##i = {                        \
-      .en = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(i), en_gpios, {0}),                \
-      .gck = GPIO_DT_SPEC_GET(DT_DRV_INST(i), gck_gpios),                      \
-      .lat = GPIO_DT_SPEC_GET(DT_DRV_INST(i), lat_gpios),                      \
-      .rst = GPIO_DT_SPEC_GET(DT_DRV_INST(i), rst_gpios),                      \
-      .selbk = GPIO_DT_SPEC_GET(DT_DRV_INST(i), selbk_gpios),                  \
-      .sin = GPIO_DT_SPEC_GET(DT_DRV_INST(i), sin_gpios),                      \
-  };                                                                           \
-                                                                               \
-  DEVICE_DT_INST_DEFINE(i, &dm163_init, NULL, NULL, &dm163_config_##i,         \
-                        POST_KERNEL, CONFIG_LED_INIT_PRIORITY, NULL);
+#define DM163_DEVICE(i)
+
+/* Build a dm163_config for DM163 peripheral with index i, named          */
+/* dm163_config_/i/ (for example dm163_config_0 for the first peripheral) */
+static const struct dm163_config dm163_config_##i = {
+    .en = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(i), en_gpios, {0}),
+    .gck = GPIO_DT_SPEC_GET(DT_DRV_INST(i), gck_gpios),
+    .lat = GPIO_DT_SPEC_GET(DT_DRV_INST(i), lat_gpios),
+    .rst = GPIO_DT_SPEC_GET(DT_DRV_INST(i), rst_gpios),
+    .selbk = GPIO_DT_SPEC_GET(DT_DRV_INST(i), selbk_gpios),
+    .sin = GPIO_DT_SPEC_GET(DT_DRV_INST(i), sin_gpios),
+};
+
+// Build a new dm163_data_/i/ structure for dynamic data
+static struct dm163_data dm163_data_##i = {};
+
+DEVICE_DT_INST_DEFINE(i, &dm163_init, NULL, NULL, &dm163_config_##i,
+                      POST_KERNEL, CONFIG_LED_INIT_PRIORITY, NULL);
 
 // Apply the DM163_DEVICE to all DM163 peripherals not marked "disabled"
 // in the device tree and pass it the corresponding index.
 DT_INST_FOREACH_STATUS_OKAY(DM163_DEVICE)
+
+// Send (bits) bits of data on the DM163
+static void pulse_data(const struct dm163_config *config, uint8_t data, int bits)
+{
+  for (int i = bits - 1; i >= 0; i--)
+  {
+    config->sin = data[i];
+    config->gck = 1; // config->gck active then inactive (clock pulse)
+    config->gck = 0;
+  }
+}
+
+//Send all channel information to bank 1 in reverse order
+static void flush_channels(const struct device *dev)
+{
+  const struct dm163_config *config = dev->config;
+  struct dm163_data *data = dev->data;
+
+  for (int i = NUM_CHANNELS - 1; i >= 0; i--) {
+    pulse_data(config, data->channels[i], 8);
+    gpio_pin_set_dt(&config->lat, 1);
+    gpio_pin_set_dt(&config->lat, 0);
+  }
+}
+
+//Send brightness information to bank 0
+static void flush_brightness(const struct device *dev) {
+
+  const struct dm163_config *config = dev->config; //retrieve config from dev
+  struct dm163_data *data = dev->data; //retrieve data from dev
+
+  gpio_pin_set_dt(&config->selbk, 0); //selection of bank 0
+
+  for (int i = NUM_CHANNELS - 1; i >= 0; i--) {
+    pulse_data(config, data->channels[i], 8);
+    gpio_pin_set_dt(&config->lat, 1);
+    gpio_pin_set_dt(&config->lat, 0);
+  }
+
+  gpio_pin_set_dt(&config->selbk, 1); //reselection of bank 1
+}
+
+//Send brightness to led
+static int dm163_set_brightness(const struct device *dev, uint32_t led, uint8_t value) {
+
+}
+
+
