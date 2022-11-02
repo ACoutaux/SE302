d21ffc54feb175420c6687ebd4e92154ca8e940b
agathe-coutaux <agathe.coutaux@telecom-paris.fr>
2022-11-02 20:39:07 +0100

    Read gyroscope (WIP)

--- tp/src/main.c
+++ tp/src/main.c
@@ -15,15 +15,15 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <math.h>
 
 #define PI 3.14159265
 
-//Structure for k_work object to read and display accelerometer value
+// Structure for k_work object to read and display accelerometer value
 struct k_work read_display_accelerometer_work;
 
-//Structure for acceleromter measurements in callback functions
+// Structure for acceleromter measurements in callback functions
 static struct gpio_callback accelerometer_data;
 
 // Define device object for distance captor vl53l0X
@@ -35,35 +35,57 @@ static struct i2c_dt_spec accelerometer = I2C_DT_SPEC_GET(DT_ALIAS(accel0));
 // Define mC irq sructure for accelerometer
 static const struct gpio_dt_spec accelerometer_irq = GPIO_DT_SPEC_GET(DT_ALIAS(accel0), irq_gpios);
 
-//Variables for accelerometer measurements
-uint8_t acc_x1, acc_x2, acc_y1, acc_y2, acc_z1, acc_z2; 
-int16_t acc_x, acc_y, acc_z, norm_acc;
-double teta_x,teta_y,teta_z;
+// Variables for accelerometer measurements
+uint8_t acc_x1, acc_x2, acc_y1, acc_y2, acc_z1, acc_z2, status, gyro_x1, gyro_x2, gyro_y1, gyro_y2, gyro_z1,gyro_z2;
+int16_t acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
+double teta_x, teta_y, teta_speed_x,teta_speed_y,teta_speed_z;
 
 void read_display_accelerometer(struct k_work *item)
 {
-	// Read linear acceleraion for X axis
-	i2c_reg_read_byte_dt(&accelerometer, 0x28, &acc_x1);
-	i2c_reg_read_byte_dt(&accelerometer, 0x29, &acc_x2);
-	// Read linear acceleraion for Y axis
-	i2c_reg_read_byte_dt(&accelerometer, 0x2a, &acc_y1);
-	i2c_reg_read_byte_dt(&accelerometer, 0x2b, &acc_y2);
-	// Read linear acceleraion for Z axis
-	i2c_reg_read_byte_dt(&accelerometer, 0x2c, &acc_z1);
-	i2c_reg_read_byte_dt(&accelerometer, 0x2d, &acc_z2);
-	// Compute linear accelerations and norm
-	acc_x = ((acc_x2 << 8) + acc_x1);
-	acc_y = ((acc_y2 << 8) + acc_y1); 
-	acc_z = ((acc_z2 << 8) + acc_z1);
+	// Read STATUS_REG register
+	i2c_reg_read_byte_dt(&accelerometer, 0x1e, &status);
 
-	//Compute angular positions
-	teta_x = atan(acc_x*0.00059857177/sqrt((pow(acc_y*0.00059857177,2)+pow(acc_z*0.00059857177,2)))) *180/PI; //converted in degrees
-	teta_y = atan(acc_y*0.00059857177/sqrt((pow(acc_x*0.00059857177,2)+pow(acc_z*0.00059857177,2)))) *180/PI;
-	printk("TiltX: %f TiltY: %f\n", teta_x,teta_y); 
+	//if (status == 7)
+	//{ // data available for both gyroscope and accelerometer
+
+		// Read gyroscope data for X axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x22, &gyro_x1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x23, &gyro_x2);
+		// Read gyroscope data for Y axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x24, &gyro_y1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x25, &gyro_y2);
+		// Read gyroscope data for Z axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x22, &gyro_z1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x23, &gyro_z2);
+		// Read linear acceleraion for X axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x28, &acc_x1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x29, &acc_x2);
+		// Read linear acceleraion for Y axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x2a, &acc_y1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x2b, &acc_y2);
+		// Read linear acceleraion for Z axis
+		i2c_reg_read_byte_dt(&accelerometer, 0x2c, &acc_z1);
+		i2c_reg_read_byte_dt(&accelerometer, 0x2d, &acc_z2);
+		// Compute linear accelerations and gyroscope measurements
+		acc_x = ((acc_x2 << 8) + acc_x1);
+		acc_y = ((acc_y2 << 8) + acc_y1);
+		acc_z = ((acc_z2 << 8) + acc_z1);
+		gyro_x = ((gyro_x2 << 8) + gyro_x1);
+		gyro_y = ((gyro_y2 << 8) + gyro_y1);
+		gyro_z = ((gyro_z2 << 8) + gyro_z1);
+
+		// Compute angular positions
+		teta_x = atan(acc_x * 0.00059857177 / sqrt((pow(acc_y * 0.00059857177, 2) + pow(acc_z * 0.00059857177, 2)))) * 180 / PI; // converted in degrees
+		teta_y = atan(acc_y * 0.00059857177 / sqrt((pow(acc_x * 0.00059857177, 2) + pow(acc_z * 0.00059857177, 2)))) * 180 / PI;
+		teta_speed_x = ((gyro_x * 250) / (2^15));
+		teta_speed_y = (gyro_y * 250) / (2^15);
+		teta_speed_z = (gyro_z * 250) / (2^15);
+		printk("TiltX: %f TiltY: %f Gx: %f Gy: %f Gz: %f\n", teta_x, teta_y,teta_speed_x,teta_speed_y,teta_speed_z);
+	//}
 }
 
 void data_on_accelerometer(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+						   uint32_t pins)
 {
 	k_work_submit(&read_display_accelerometer_work);
 }
@@ -72,7 +94,7 @@ void main(void)
 {
 	// Variable for interrupt line
 	int ret;
-	//Init work object and associate with read and display data function
+	// Init work object and associate with read and display data function
 	k_work_init(&read_display_accelerometer_work, read_display_accelerometer);
 
 	// Check if accelerometer is ready
@@ -82,7 +104,7 @@ void main(void)
 		return;
 	}
 
-	//Set interruption line as input
+	// Set interruption line as input
 	ret = gpio_pin_configure_dt(&accelerometer_irq, GPIO_INPUT);
 	if (ret != 0)
 	{
@@ -100,12 +122,16 @@ void main(void)
 		return;
 	}
 
-	//Set callback function on accelerometer
+	// Set callback function on accelerometer
 	gpio_init_callback(&accelerometer_data, data_on_accelerometer, BIT(accelerometer_irq.pin));
 	gpio_add_callback(accelerometer_irq.port, &accelerometer_data);
 
+	// Set accelerometer characteristics
 	i2c_reg_write_byte_dt(&accelerometer, 0x12, 0b00000101); // reset accelerometer and keep automatic incrementation of adresses enabled (CTRL3_C)
 	i2c_reg_write_byte_dt(&accelerometer, 0x15, 0b00010000); // set XL_HM_MODE to 1 to disable high performance mode
 	i2c_reg_write_byte_dt(&accelerometer, 0x10, 0b10110000); // set ODR_XLR[3:0] to 1 0 1 1 for a frequency at 1.6 Hz and FS_XL[1:0] to 0 0 to have measurements beetween -2g(0) and 2g(65535)
+	// Set gyroscope characteristics
+	i2c_reg_write_byte_dt(&accelerometer, 0x11, 0b01000000); // set measurements frequency for gyroscope at 104 hz (CTRL2_G register)
+	// Set interruption characteristics
 	i2c_reg_write_byte_dt(&accelerometer, 0x0d, 0b00000001); // set INT1_DRDY_XL (data on acceleromter) to 1 (enable) on the interruption register INT1_CTRL
 }
