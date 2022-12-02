d6cef04feec3dc3e351160136e2bcf24bad7d5bb
Agathe Coutaux <agathe.coutaux@telecom-paris.fr>
2022-12-02 14:18:44 +0100

    Add step 2.4 to history

--- pwdchk/src/account.rs
+++ pwdchk/src/account.rs
@@ -1,4 +1,5 @@
 //! This module implements account (login + password) structure
+use std::collections::HashMap;
 use std::str::FromStr;
 
 #[derive(Clone, Debug)] //with this line debug trait can be used by Account structure
@@ -12,6 +13,21 @@ pub struct NoColon;
 
 ///Implements Account structure associated functions
 impl Account {
+    ///Builds a hasmap with password keys and login values from an accounts vector
+    pub fn group(accounts: Vec<Account>) -> HashMap<String, Vec<String>> {
+        let mut accounts_map: HashMap<String, Vec<String>> = HashMap::new();
+        for account in accounts.iter() {
+            let mut current_log: Vec<String> = vec![];
+            current_log.push(account.login.clone());
+            accounts_map
+                .entry(account.password.clone())
+                .and_modify(|e| e.push(account.login.clone()))
+                .or_insert(current_log);
+        }
+        accounts_map.retain(|_, v| v.len() > 1);
+        accounts_map
+    }
+
     ///Constructor for Account structure
     pub fn new(login: &str, password: &str) -> Self {
         Account {
@@ -22,20 +38,18 @@ impl Account {
 
     ///Builds and returns an account from a string
     pub fn from_string(s: &str) -> Self {
-      Account::new(s.split_once(':').unwrap().0, s.split_once(':').unwrap().1)
+        Account::new(s.split_once(':').unwrap().0, s.split_once(':').unwrap().1)
     }
-  
 }
 
 impl FromStr for Account {
-  type Err = NoColon;
-  ///Returns an Account structure if input string contains ':' and an error otherwise
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-      if s.contains(':') {
-          Ok(Self::from_string(s))
-      } else {
-          Err(NoColon)
-      }
-  }
+    type Err = NoColon;
+    ///Returns an Account structure if input string contains ':' and an error otherwise
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.contains(':') {
+            Ok(Self::from_string(s))
+        } else {
+            Err(NoColon)
+        }
+    }
 }
-
--- pwdchk/src/main.rs
+++ pwdchk/src/main.rs
@@ -6,7 +6,13 @@ use std::str::FromStr;
 
 fn main() -> Result<(), NoColon> {
     let v = std::env::args().skip(1).map(|x| Account::from_str(x.as_str())).collect::<Result<Vec<_>,_>>()?;
-    println!("{:#?}",v);
+    let hash = Account::group(v);
+    //Print passwords with associated logins
+    for key in hash.keys() {
+        let logins = hash.get(key).unwrap().join(", ");
+        println!("Password {} used by {}", key, logins);
+    }
+
     Ok(())
 }
   
