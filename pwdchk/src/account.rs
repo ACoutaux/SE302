de4a1ea0fc1d6dc91965ead4450ab8c52d854195
Agathe Coutaux <agathe.coutaux@telecom-paris.fr>
2022-12-02 10:17:20 +0100

    Add step 2.1 to history

--- pwdchk/src/account.rs
+++ pwdchk/src/account.rs
@@ -1,58 +1,18 @@
-//! This module implements account (login + password) structure 
-use super::error; //to use content of error module in this module
-use std::str::FromStr;
-use std::collections::HashMap;
-use std::path::Path;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+//! This module implements account (login + password) structure
 
-#[derive(Clone)]
-#[derive(Debug)] //with this line debug trait can be used by Account structure
-pub struct Account{
+#[derive(Clone, Debug)] //with this line debug trait can be used by Account structure
+pub struct Account {
     login: String,
     password: String,
 }
 
+///Implements Account structure associated functions
 impl Account {
-  pub fn new(login: &str, password: &str) -> Self {
-    Account {login: login.to_string(), password: password.to_string() }
-  }
-
-  pub fn from_string(s: &str) -> Self {
-    Account::new(s.split_once(':').unwrap().0, s.split_once(':').unwrap().1)
-  }
-
-  pub fn group(accounts: Vec<Account>) -> HashMap<String, Vec<String>> {
-    let mut accounts_map: HashMap<String, Vec<String>> = HashMap::new();
-    for account in accounts.iter() {
-      let mut current_log: Vec<String> = vec![];
-      current_log.push(account.login.clone());
-      accounts_map.entry(account.password.clone()).and_modify(|e|{e.push(account.login.clone())}).or_insert(current_log);
-    }
-    accounts_map.retain(|_,v| {v.len()>1});
-    accounts_map
-  }
-
-  ///Load accounts from text file (one account per line)
-  pub fn from_file(filename: &Path) -> Result<Vec<Account>, error::Error> {
-    let f = File::open(Path::new(filename))?; //open file indicated by path argument and returns error otherwise
-    let f = BufReader::new(f); //declare read buffer on file (mask previous f value)
-    let mut accounts: Vec<Account> = vec![]; //init accounts vector
-    for line in f.lines() {
-      accounts.push(Self::from_str(&line.unwrap()).unwrap())
-    }
-    Ok(accounts) 
-  }
-}
-
-impl FromStr for Account {
-    type Err = error::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.contains(':') {
-            Ok(Self::from_string(s))
-        } else {
-            Err(error::Error::NoColon)
+    ///Constructor for Account structure
+    pub fn new(login: &str, password: &str) -> Self {
+        Account {
+            login: login.to_string(),
+            password: password.to_string(),
         }
     }
 }
--- pwdchk/src/main.rs
+++ pwdchk/src/main.rs
@@ -1,63 +1,10 @@
 //! Main function module
 
 mod account; //reference to account module
-mod error;
+use account::*; //use impletentations of account module
 
-use account::*;
-use clap::{Args, Parser, Subcommand, ArgGroup};
-use std::path::PathBuf;
-
-#[derive(Parser)]
-#[clap(version, author, about)]
-struct AppArgs {
-    #[clap(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand)]
-enum Command {
-    /// Check duplicate passwords from command line
-    Group(GroupArgs),
-}
-
-#[derive(Args)]
-#[clap(group(
-    ArgGroup::new("input")
-        .required(true)
-        .args(&["account", "file"]),
-))]
-struct GroupArgs {
-    #[clap(required = false)]
-    /// Account to check
-    account: Vec<Account>,
-    #[clap(short, long)]
-    /// Load passwords from a file
-    file: Option<PathBuf>,
-}
-
-fn main() -> Result<(), error::Error> {
-    let args = AppArgs::parse(); //get command line arguments
-    match args.command {
-        Command::Group(args) => {  
-            match args.file {
-                Some(path_) => {
-                    let accounts = Account::from_file(&path_);
-                    let hash = Account::group(accounts.unwrap());
-                    for key in hash.keys() {
-                        let logins = hash.get(key).unwrap().join(", ");
-                        println!("Password {} used by {}", key, logins);
-                    }
-                },
-                None => {
-                    let hash = Account::group(args.account);
-                    for key in hash.keys() {
-                        let logins = hash.get(key).unwrap().join(", ");
-                        println!("Password {} used by {}", key, logins);
-                    }
-                }
-            }
-        }
-    }
-    Ok(())
+fn main() {
+    let account = Account::new("johndoe", "super:complex:password");
+    println!("account = {account:#?}");
 }
   
