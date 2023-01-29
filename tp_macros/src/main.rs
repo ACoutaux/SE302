dd92aff118a33f1787c6b657e80a4c80a968d87d
agathe-coutaux <agathe.coutaux@telecom-paris.fr>
2023-01-29 18:19:23 +0100

    Step 6.1 completed

--- tp_macros/src/main.rs
+++ tp_macros/src/main.rs
@@ -4,7 +4,15 @@ macro_rules! cartesian {
     ($U:expr,$V:expr) => {{
         let u = $V.into_iter().collect::<Vec<_>>(); //collect the second member expressions in a vector in order to not compute values several times
         $U.into_iter().flat_map(move |l| u.clone().into_iter().map(move |r| (l.clone(),r))) //returns iterations of combinations of U and V
-    }}
+    }};
+    //Option with 3 inputs
+    ($U:expr, $V:expr, $($W:expr),+) => {
+        {
+            let U = $U.into_iter();
+            let V = cartesian!($V, $($W),+);
+            U.flat_map(move |x| V.clone().map(move |y| (x.clone(), y)))
+        }
+    };
 }
 
 fn main() {
